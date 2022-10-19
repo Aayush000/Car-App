135358a9c9e81219443625ed24d56ef512f11d22
Aayush <aayushbhatta000@gmail.com>
2022-10-19 14:52:20 -0500

    Added the funcitonality to post, edit, and delete.

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,145 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { json } = require("express");
+const app = express();
 
-/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
- * import express */
+// To support cors.
+app.use(cors());
 
-let carsMockData = [
-    {
-        "id": 1,
-        "brand": "Hyundai",
-        "name": "Ioniq",
-        "releaseYear": 2017,
-        "color": "blue"
-    },
-    {
-        "id": 2,
-        "brand": "Toyota",
-        "name": "Prius",
-        "releaseYear": 2007,
-        "color": "blue"
-    },
-    {
-        "id": 3,
-        "brand": "Chevrolet",
-        "name": "Aveo",
-        "releaseYear": 2007,
-        "color": "white"
-    },
-    {
-        "id": 4,
-        "brand": "BMW",
-        "name": "M5",
-        "releaseYear": 2017,
-        "color": "White"
-    },
-    {
-        "id": 5,
-        "brand": "Tesla",
-        "name": "S",
-        "releaseYear": 2019,
-        "color": "Black"
-    }
-]
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Car App.");
+});
 
 /** Create GET API. API shoudl return  const carsMockData*/
+app.get("/carsList", (req, res) => {
+  fs.readFile("./carsMockData.json", (err, carsList) => {
+    if (err) {
+      console.log(err);
+    }
+    const carsMockData = JSON.parse(carsList);
+    res.json(carsMockData);
+  });
+});
 
-
-
-
-
-/** Create POST API. Get the new car data from react. 
+/** Create POST API. Get the new car data from react.
  *      Check if car with id exists. If Yes return 500. With message 'Car already exists'
  *      If there is no car with the id, add the new car to  carsMockData and return carsMockData as response */
+app.post("/save", (req, res) => {
+  fs.readFile("./carsMockData.json", "utf8", (err, carsList) => {
+    if (err) {
+      console.log("File read failed: ", err);
+    }
+    let id = req.body.id;
+    let brand = req.body.brand;
+    let name = req.body.name;
+    let releaseYear = req.body.releaseYear;
+    let color = req.body.color;
 
+    let json = JSON.parse(carsList);
 
+    json.push({
+      id: id,
+      brand: brand,
+      name: name,
+      releaseYear: releaseYear,
+      color: color,
+    });
+    fs.writeFile(
+      "./carsMockData.json",
+      JSON.stringify(json),
+      (err, carsList) => {
+        if (err) {
+          console.log("File write Failed: ", err);
+        }
+      }
+    );
+    res.sendStatus(200);
+  });
+});
 
-
-
-/** Create PUT API. 
- *  Check if car with id exists. If No return 500 with error 'No car with given id exist'. 
+/** Create PUT API.
+ *  Check if car with id exists. If No return 500 with error 'No car with given id exist'.
  *  If there is car with the requested id, update that car's data in 'carsMockData' and return 'carsMockData' */
+app.put("/edit", (req, res) => {
+  fs.readFile("./carsMockData.json", "utf8", (err, carsList) => {
+    if (err) {
+      console.log("File read Failed: ", err);
+    }
 
+    let id = req.body.id;
 
+    let json = JSON.parse(carsList);
 
+    let result = json.map((car) => {
+      if (car.id == id) {
+        car.brand = req.body.brand;
+        car.name = req.body.name;
+        car.releaseYear = req.body.releaseYear;
+        car.color = req.body.color;
+      }
+      return car;
+    });
 
-/** Create Delete API. 
+    fs.writeFile(
+      "./carsMockData.json",
+      JSON.stringify(result),
+      (err, carsList) => {
+        if (err) {
+          console.log("File write Failed: ", err);
+        }
+      }
+    );
+    res.sendStatus(200);
+  });
+});
+
+/** Create Delete API.
  *  Check if car with id exists. If No return 500. With message 'No car with give id exists'
  *  If there is car with the requested id. Delete that car from 'carsMockData' and return 'carsMockData'
-*/
+ */
+app.delete("/delete", (req, res) => {
+  fs.readFile("./carsMockData.json", "utf8", (err, carsList) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+
+    let json = JSON.parse(carsList);
+    let id = req.body.id;
+    let result = json.filter((carDetails) => carDetails.id != id);
+
+    fs.writeFile(
+      "./carsMockData.json",
+      JSON.stringify(result),
+      (err, carsList) => {
+        if (err) {
+          console.log("File write failed:", err);
+          return;
+        }
+      }
+    );
+    res.sendStatus(200);
+  });
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,161 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+// Initial Car Details
+const carFormInitialData = {
+  id: null,
+  brand: "",
+  name: "",
+  releaseYear: "",
+  color: "",
+};
 
 function Cars() {
-/**
- * Fields required for the car
-      "id",
-      "brand",
-      "name",
-      "releaseYear",
-      "color"
- */
-  const carFormInitialData = {
-    id: 0,
-    name: ''
-  }
+  // Statte that stores the carFormData.
   const [carFormData, setCarFormData] = useState(carFormInitialData);
+  // State that store the list of Cars.
+  const [carList, setCarList] = useState([]);
+  // State that refresh the page and update the page with new information.
+  const [refresh, setRefresh] = useState(false);
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const { name, brand, releaseYear, color, value } = e.target;
     setCarFormData({
       ...carFormData,
       [name]: value,
+      [brand]: value,
+      [releaseYear]: value,
+      [color]: value,
     });
-  }
+  };
 
   const handleSubmit = (event) => {
-    /**
-     * Gather all the form data to state variable carFormData
-     * When the form is submitted POST the data to Backend using fetch post
-     * https://googlechrome.github.io/samples/fetch-api/fetch-post.html
-     */
     event.preventDefault();
-  }
+    let submit = document.getElementById("submit");
 
-  const handleDelete = () => {
-    /**
-     * When clicked on a delete button, get the id of the car's delete button clicked
-     * Then use javascript fetch to send DELETE request to NodeJS
-     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
-     */
-  }
+    if (submit.value == "Update") {
+      handleEdit(event);
+    } else {
+      handlePost(event);
+    }
+    event.target.reset();
+  };
 
-/** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
-  const handleEdit = () => {
+  const handlePost = async (event) => {
+    let res = await fetch("http://localhost:3002/save", {
+      method: "POST",
+      body: JSON.stringify({
+        name: event.target.name.value,
+        id: event.target.id.value,
+        brand: event.target.brand.value,
+        releaseYear: event.target.releaseYear.value,
+        color: event.target.color.value,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => setRefresh(!refresh));
+  };
+
+  const handleDelete = async (event) => {
+    let res = await fetch("http:/localhost:3002/delete", {
+      method: "DELETE",
+      body: JSON.stringify({
+        id: event.target.parentElement.id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => setRefresh(!refresh));
+  };
+
+  /** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
+  const handleEdit = async (event) => {
     /**
      * When clicked on a edit button figure out a way to edit the car data.
      * Once edited send the updated data to NodeJS.
      * Then use javascript fetch to send DELETE request to NodeJS
      * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
      */
-  }
- 
+    event.preventDefault();
+    let id = document.getElementById("id");
+    let name = document.getElementById("name");
+    let brand = document.getElementById("brand");
+    let releaseYear = document.getElementById("releaseYear");
+    let color = document.getElementById("color");
+    let submit = document.getElementById("submit");
+    submit.value = "Update";
+
+    id.value = event.target.parentElement.getAttribute("id");
+    name.value = event.target.parentElement.getAttribute("name");
+    brand.value = event.target.parentElement.getAttribute("brand");
+    releaseYear.value = event.target.parentElement.getAttribute("releaseYear");
+    color.value = event.target.parentElement.getAttribute("color");
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3002/carsList")
+      .then((res) => res.json())
+      .then((result) => {
+        setCarList(result);
+      });
+  }, [refresh]);
+
   return (
-    <div className='cars-from-wrapper'>
+    <div className="cars-from-wrapper">
       <form id="cars-form" onSubmit={handleSubmit} autoComplete="off">
-        {/** 
-           * TODO: Update the form fields with inputs for 
-           *    ID, Brand, Name, ReleaseYear and Color
-           * Make required changes to  const carFormInitialData
-           * */}  
         <label>
           ID:
-          <input name='id' type="text" value={carFormData.id} onChange={handleInputChange} />
+          <input
+            name="id"
+            type="text"
+            value={carFormData.id}
+            onChange={handleInputChange}
+          />
         </label>
         <label>
           Name:
-          <input name='name' type="text" value={carFormData.name} onChange={handleInputChange} />
+          <input
+            name="name"
+            type="text"
+            value={carFormData.name}
+            onChange={handleInputChange}
+          />
         </label>
-        <input type="submit" value="Submit" />
+        <label>
+          Brand:
+          <input
+            name="brand"
+            type="text"
+            value={carFormData.brand}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Release Year:
+          <input
+            name="releaseYear"
+            type="text"
+            value={carFormData.releaseYear}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Color:
+          <input
+            name="color"
+            type="text"
+            value={carFormData.color}
+            onChange={handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Submit" id="submit" />
       </form>
-       {/** 
-           * TODO: Update the code below to see any new proprties added to carFormData
-           * */}  
-      <p>ID:{carFormData.id}, name:{carFormData.name}</p>
+
+      <p>
+        ID:{carFormData.id} --- Name:{carFormData.name} --- Brand:
+        {carFormData.brand} --- Relese Year: {carFormData.releaseYear} ---
+        Color: {carFormData.color}
+      </p>
 
       <h2>Cars Data</h2>
+
       <table>
         <thead>
           <tr>
@@ -86,53 +167,35 @@ function Cars() {
           </tr>
         </thead>
         <tbody>
-          {/** 
-           * TODO: Replace this code with Data from Node JS GET api data
-           * React documentation: https://reactjs.org/docs/lists-and-keys.html
-           * How to get data from API: https://www.w3schools.com/jsref/api_fetch.asp
-           * */}          
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>✎</td>
-            <td>🗑</td>        
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
+          {carList.map((car) => {
+            return (
+              <tr key={car.id}>
+                <td>{car.id}</td>
+                <td>{car.name}</td>
+                <td>{car.brand}</td>
+                <td
+                  id={car.id}
+                  name={car.name}
+                  brand={car.brand}
+                  releaseYear={car.releaseYear}
+                  color={car.color}
+                  onClick={handleEdit}
+                >
+                  ✎
+                </td>
+                <td
+                  id={car.id}
+                  name={car.name}
+                  brand={car.brand}
+                  releaseYear={car.releaseYear}
+                  color={car.color}
+                  onClick={handleDelete}
+                >
+                  🗑
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
